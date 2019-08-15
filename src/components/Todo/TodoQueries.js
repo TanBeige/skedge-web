d301e7b2fced467f49718d8bb5c0ed64d7f7061c
Tan arin <tanarin12@gmail.com>
2019-08-15 14:09:31 -0400

    init commit
    i dont even know man, ive made so many repos ...

---
+++ src/components/Todo/TodoQueries.js
@@ -0,0 +1,135 @@
+import gql from "graphql-tag";
+
+const EVENT_FRAGMENT = gql`
+  fragment TodoFragment on events {
+    id
+    description
+    created_at
+    is_public
+  }
+`;
+
+const USER_FRAGMENT = gql`
+  fragment UserFragment on entity {
+    username
+  }
+`;
+
+const QUERY_PRIVATE_EVENT = gql`
+  query events($userId: String!) {
+    events(
+      where: { is_public: { _eq: false }, creator_id: { _eq: $userId } }
+      order_by: { created_at: desc }
+    ) {
+      ...TodoFragment
+    }
+  }
+  ${EVENT_FRAGMENT}
+`;
+
+const QUERY_PUBLIC_EVENT = gql`
+  query fetch_events($eventLimit: Int, $eventId: Int) {
+    events(
+      where: { is_public: { _eq: true }, id: { _gt: $eventId } }
+      order_by: { created_at: desc }
+      limit: $eventLimit
+    ) {
+      ...TodoFragment
+      user {
+        ...UserFragment
+      }
+    }
+  }
+  ${EVENT_FRAGMENT}
+  ${USER_FRAGMENT}
+`;
+
+const QUERY_FEED_PUBLIC_EVENT = gql`
+  query fetch_events($eventId: Int) {
+    events(
+      where: { is_public: { _eq: true }, id: { _gt: $eventId } }
+      order_by: { created_at: desc }
+    ) {
+      ...TodoFragment
+      user {
+        ...UserFragment
+      }
+    }
+  }
+  ${EVENT_FRAGMENT}
+  ${USER_FRAGMENT}
+`;
+
+const QUERY_FEED_PUBLIC_OLD_EVENT = gql`
+  query fetch_events($eventId: Int) {
+    events(
+      where: { is_public: { _eq: true }, id: { _lt: $eventId } }
+      limit: 5
+      order_by: { created_at: desc }
+    ) {
+      ...TodoFragment
+      user {
+        ...UserFragment
+      }
+    }
+  }
+  ${EVENT_FRAGMENT}
+  ${USER_FRAGMENT}
+`;
+
+const MUTATION_EVENT_ADD = gql`
+  mutation insert_events($objects: [events_insert_input!]!) {
+    insert_events(objects: $objects) {
+      affected_rows
+      returning {
+        id
+        description
+        updated_at
+        is_public
+      }
+    }
+  }
+`;
+
+const MUTATION_EVENT_UPDATE = gql`
+  mutation update_events($eventId: Int, $set: events_set_input!) {
+    update_events(where: { id: { _eq: $eventId } }, _set: $set) {
+      affected_rows
+    }
+  }
+`;
+
+const MUTATION_EVENT_DELETE = gql`
+  mutation delete_events($eventId: Int) {
+    delete_events(where: { id: { _eq: $eventId } }) {
+      affected_rows
+    }
+  }
+`;
+
+const SUBSCRIPTION_EVENT_PUBLIC_LIST = gql`
+  subscription($eventId: Int) {
+    events(
+      where: { is_public: { _eq: true }, id: { _gt: $eventId } }
+      order_by: { created_at: desc }
+      limit: 1
+    ) {
+      id
+      text
+      is_completed
+      created_at
+      is_public
+    }
+  }
+`;
+
+export {
+  QUERY_PRIVATE_EVENT,
+  QUERY_PUBLIC_EVENT,
+  QUERY_FEED_PUBLIC_EVENT,
+  QUERY_FEED_PUBLIC_OLD_EVENT,
+  MUTATION_EVENT_ADD,
+  MUTATION_EVENT_UPDATE,
+  MUTATION_EVENT_DELETE,
+  SUBSCRIPTION_EVENT_PUBLIC_LIST
+};
---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+const HASURA_GRAPHQL_ENGINE_HOSTNAME = "skedge-web.herokuapp.com";
+
+const scheme = proto => {
+  return window.location.protocol === "https:" ? `${proto}s` : proto;
+};
+
+export const GRAPHQL_URL = `${scheme(
+  "http"
+)}://${HASURA_GRAPHQL_ENGINE_HOSTNAME}/v1/graphql`;
+export const REALTIME_GRAPHQL_URL = `${scheme(
+  "ws"
+)}://${HASURA_GRAPHQL_ENGINE_HOSTNAME}/v1/graphql`;
+export const authClientId = "RH47tlmwwW28QH44WrohPJ1YyjAAEEbd";
+export const authDomain = "skedge.auth0.com";
+export const callbackUrl = `http://localhost:3000/callback`;
+
+
+//https://skedge.auth0.com/login?client=RH47tlmwwW28QH44WrohPJ1YyjAAEEbd&protocol=oauth2&response_type=token%20id_token&redirect_uri=https://localhost:3000/callback&scope=openid%20profile
