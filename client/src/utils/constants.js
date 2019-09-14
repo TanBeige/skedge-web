e2fa2cd8ee1fe66a53e1ebfa1b286be28141dabc
Tan arin <tanarin12@gmail.com>
2019-09-14 18:49:26 -0400

    Change Vairbales For Heroku

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -14,10 +14,12 @@ export const REALTIME_GRAPHQL_URL = `${scheme(
 )}://${HASURA_GRAPHQL_ENGINE_HOSTNAME}/v1/graphql`;
 export const authClientId = "RH47tlmwwW28QH44WrohPJ1YyjAAEEbd";
 export const authDomain = "skedge.auth0.com";
-//export const webUrl = `https://secret-journey-62331.herokuapp.com`;
+
+export const webUrl = `https://secret-journey-62331.herokuapp.com`;
 //export const webUrl = 'http://192.168.0.5:3000'
-export const webUrl = `http://localhost:3000`;
+//export const webUrl = `http://localhost:3000`;
 export const callbackUrl = `${webUrl}/callback`;
-export const backendUrl = 'http://localhost:9000';
+//export const backendUrl = 'http://localhost:9000';
+export const backendUrl = `https://secret-journey-62331.herokuapp.com:9000`;
+
 export const s3Url = process.env.S3URL;
-//export const backendUrl = 'http://192.168.0.5:9000'
